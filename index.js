76b01d6af201dc09c74450c100e451736117f211
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-10 06:07:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = "70"
+=======
+var name = "Joe"
+var height = "74"
+>>>>>>> 827fe77a47ed9b8c2e83c3975063899964bb9c10
 
 // Don't worry about this
 module.exports = { name, height
--- other_file.js
+++ other_file.js
@@ -1,2 +1,6 @@
 var index = require("./index.js")
+<<<<<<< HEAD
+=======
+
+>>>>>>> 827fe77a47ed9b8c2e83c3975063899964bb9c10
 console.log(index.name)
